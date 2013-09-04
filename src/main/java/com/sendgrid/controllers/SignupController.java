cc429595e061bd6d7a77afd871b6410af2403186
scottmotte <scott@scottmotte.com>
2013-09-04 16:10:11 -0700

    Update to use 0.1.0

--- src/main/java/com/sendgrid/controllers/SignupController.java
+++ src/main/java/com/sendgrid/controllers/SignupController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.sendgrid.forms.Signup;
-import com.github.scottmotte.sendgrid.SendGrid;
+import com.github.sendgrid.SendGrid;
+import com.github.scottmotte.Vcapenv;
+import java.lang.System;
 
 @Controller
 public class SignupController {
@@ -15,18 +17,32 @@ public class SignupController {
   @RequestMapping("/")
   public String index(Model model) {
     model.addAttribute("signup", new Signup());
+    Vcapenv vcapenv   = new Vcapenv();
+
+    System.out.println(vcapenv.getCurrentNode());
+    System.out.println(vcapenv.getOriginalNode());
+
+    String password   = vcapenv.SENDGRID_PASSWORD();
+
+    System.out.println(password);
+    System.out.println(vcapenv.getCurrentNode());
+    System.out.println(vcapenv.getOriginalNode());
+
+    String username   = vcapenv.SENDGRID_USERNAME();
+    System.out.println(username);
+    
 
     return "index";
   }
 
   @RequestMapping(value = "/signup", method = RequestMethod.POST)
   public String postSignup(@ModelAttribute Signup signup, Model model) {
-    System.out.println(signup.getEmail());
-    
-    String username   = System.getenv("SENDGRID_USERNAME");
-    String password   = System.getenv("SENDGRID_PASSWORD");
-    SendGrid sendgrid = new SendGrid(username, password);
 
+    Vcapenv vcapenv   = new Vcapenv();
+    String username   = vcapenv.SENDGRID_USERNAME();
+    String password   = vcapenv.SENDGRID_PASSWORD();
+
+    SendGrid sendgrid = new SendGrid(username, password);
     sendgrid.addTo(signup.getEmail());
     sendgrid.setFrom("scott.motte@sendgrid.com");
     sendgrid.setSubject("[Spring Attack] Welcome");
